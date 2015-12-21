359e8b68ff08b072aa36a9a467f07dc0ed5d7de5
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-12-20 16:29:41 -0800

    show page now works with resume-api, albeit without much style, just using ul/li tags

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -3,7 +3,7 @@ class CatalogsController < ApplicationController
 
   def show
 
-    @error_message
+    @error_message = []
 
     id = params[:id]
 
@@ -11,29 +11,6 @@ class CatalogsController < ApplicationController
 
     @error_message << "Student capstone not found!" unless @student
 
-    #capstones_hash = Unirest.get("http://localhost:3000/capstones/index").body
-
-    #@capstone = find_student_capstone(capstones_hash, id)
-    
-    #@error_msg << "Student capstone not found!" unless @capstone
-
   end
-
-
-  # private
-
-  # def find_student_capstone (capstones_hash, student_id)
-
-  #   capstones_hash.each do |capstone|
-
-  #     return capstone if capstone["student_id"]==id
-  #     end
-
-  #   end
-
-  #   return nil
-
-  # end
-
 
 end
