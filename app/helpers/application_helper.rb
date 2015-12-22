65a4cf376903f13be4fe5a955a1dbf8694d3ce34
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-12-21 19:57:39 -0800

    added a table and rand bg color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def rand_bgc_hex
+    r = rand(255).to_s(16)
+    g = rand(255).to_s(16)
+    b = rand(255).to_s(16)
+    r, g, b = [r, g, b].map { |s| if s.size == 1 then '0' + s else s end }
+    return r + g + b 
+  end
+
+
 end
+
