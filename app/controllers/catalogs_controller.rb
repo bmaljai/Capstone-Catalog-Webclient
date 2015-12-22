46f11b56049f08c9752dbfa726fbfea008025952
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-12-21 20:33:54 -0800

    added PORO functionality to index action/page

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,7 +1,7 @@
 class CatalogsController < ApplicationController
 
   def index
-    @capstones = Unirest.get("http://localhost:3000/students.json").body
+    @catalogs = Catalog.all
   end
 
   def show
--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,8 +1,9 @@
 class Catalog
 
-  attr_accessor :first_name, :last_name, :capstone_name, :capstone_desc, :capstone_url, :capstone_screen
+  attr_accessor :id, :first_name, :last_name, :capstone_name, :capstone_desc, :capstone_url, :capstone_screen
 
   def initialize(hash)
+    @id=hash["id"]
     @first_name=hash["first_name"]
     @last_name=hash["last_name"]
     @capstone_name=hash["capstone"]["name"]
@@ -13,6 +14,15 @@ class Catalog
 
   def self.find(id)
     Catalog.new(Unirest.get("http://localhost:3000/students/#{id}").body)
+  end
+
+  def self.all
+    catalogs=[]
+    json_array = Unirest.get("http://localhost:3000/students.json").body
+    json_array.each do |json_elem|
+      catalogs<<Catalog.new(json_elem)
+    end
+    catalogs
   end
 
 end
